9b4136896811917aa724b1f1df5d6ed72197f286
david.tamurejo@ciberexperis.es <david.tamurejo@gmail.com>
2017-11-02 13:28:33 +0100

    asfdaSD

---
+++ PruebaMaven/src/main/java/pruebaMaven/PruebaMaven/App.java
@@ -0,0 +1,13 @@
+package pruebaMaven.PruebaMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
